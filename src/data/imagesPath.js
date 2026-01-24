686674acf091a1f6da86eb339359981ef834f2b9
Mingx1994x <mggxmingx@gmail.com>
2026-01-24 16:08:27 +0800

    chore: 新增圖片路徑模組以管理專案中的靜態圖片資源

---
+++ src/data/imagesPath.js
@@ -0,0 +1,60 @@
+import logoUrl from '/images/Logo.png'
+import logoNavbarUrl from '/images/Logo-navbar.svg'
+
+// home
+import imgHomeURl1 from '/images/home-1.jpg'
+import imgHomeURl2 from '/images/home-2.jpg'
+import imgHomeURl3 from '/images/home-3.jpg'
+import imgHomeURl4 from '/images/home-4.jpg'
+import imgHomeURl5 from '/images/home-5.jpg'
+import imgHomeURl6 from '/images/home_event-6.jpg'
+import imgHomePathURl from '/images/path.png'
+import imgHomePathURl_sm from '/images/path-sm.png'
+
+// about
+import imgAboutURl1 from '/images/about-1.jpg'
+import imgAboutURl2 from '/images/about-2.jpg'
+
+// account
+import imgAccountURl1 from '/images/account-posts-1.jpg'
+import imgAccountURl2 from '/images/account-posts-2.jpg'
+import imgAccountURl3 from '/images/account-posts-3.jpg'
+import imgAccountURl4 from '/images/account-posts-4.jpg'
+import imgAccountURl5 from '/images/account-posts-5.jpg'
+
+// event
+import imgEventURl1 from '/images/event-1.jpg'
+import imgEventURl2 from '/images/event-2.jpg'
+
+// guide
+import imgGuideBannerURl from '/images/guide-banner.jpg'
+import imgGuideContactURl1 from '/images/guide-contact-1.jpg'
+import imgGuideContactURl2 from '/images/guide-contact-2.jpg'
+
+
+
+
+export {
+  logoUrl,
+  logoNavbarUrl,
+  imgHomeURl1,
+  imgHomeURl2,
+  imgHomeURl3,
+  imgHomeURl4,
+  imgHomeURl5,
+  imgHomeURl6,
+  imgHomePathURl,
+  imgHomePathURl_sm,
+  imgAboutURl1,
+  imgAboutURl2,
+  imgAccountURl1,
+  imgAccountURl2,
+  imgAccountURl3,
+  imgAccountURl4,
+  imgAccountURl5,
+  imgEventURl1,
+  imgEventURl2,
+  imgGuideBannerURl,
+  imgGuideContactURl1,
+  imgGuideContactURl2
+}
