1dee34ca24a56c3ca6643ff4f1afa0199a95bc5f
Mingx1994x <mggxmingx@gmail.com>
2026-01-24 22:33:34 +0800

    refactor: 拆分 Home 頁面 Yummy 區塊元件，並調整圖片路徑

--- src/data/imagesPath.js
+++ src/data/imagesPath.js
@@ -31,7 +31,9 @@ import imgGuideBannerURl from '/images/guide-banner.jpg'
 import imgGuideContactURl1 from '/images/guide-contact-1.jpg'
 import imgGuideContactURl2 from '/images/guide-contact-2.jpg'
 
-
+// deco
+import decoArrowPinkUrl1 from '/images/deco/arrow-pink-1.png'
+import decoArrowPinkUrl2 from '/images/deco/arrow-pink-2.png'
 
 
 export {
@@ -56,5 +58,7 @@ export {
   imgEventURl2,
   imgGuideBannerURl,
   imgGuideContactURl1,
-  imgGuideContactURl2
+  imgGuideContactURl2,
+  decoArrowPinkUrl1,
+  decoArrowPinkUrl2
 }
